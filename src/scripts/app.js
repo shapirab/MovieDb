30bd13ea9b0d5995b83e94708b239ff1df9e600c
shapirab <shapirab@jamd.ac.il>
2024-06-10 07:51:56 +0300

    10/6/24 generated the movie cards programmatically in app.js without testing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,18 +3,13 @@ import { HttpService } from "./http.js";
 const httpService = new HttpService();
 const cardsContainer = document.getElementById('cards-container');
 
-// let movies = httpService.getAllMovies();
-
-// movies.forEach(movie => {
-//     console.log(movie);
-// });
-
 (async() => {
     try{
         let movies = await httpService.getAllMovies();
         if(Array.isArray(movies)){
             movies.forEach(movie => {
-                console.log(movie);
+                generateMovieCard(movie);
+                
             });            
         }
         else{
@@ -25,4 +20,44 @@ const cardsContainer = document.getElementById('cards-container');
         console.error('Error fetching movies: ', error);
     }
 })();
+
+function generateMovieCard(movie){
+    let cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
+
+    let movieCard = document.createElement('div');
+    movieCard.classList.add('card--movie');
+
+    let movieImage = document.createElement('div');
+    movieImage.classList.add('card--movie__image');
+
+    let imageElement = document.createElement('img');
+    imageElement.src = `data:image/png;base64,${movie.Cover}`;
+
+    movieImage.appendChild(imageElement);
+    movieCard.appendChild(movieImage);
+
+    let movieInfo = document.createElement('div');
+    movieInfo.classList.add('movie__info');
+
+    let movieHeader = document.createElement('div');
+    movieHeader.classList.add('movie-card--header');
+
+    let movieHeading = document.createElement('h2');
+    movieHeading.classList.add('movie-card__heading');
+    movieHeading.innerText = movie.Title;
+
+    let movieRating = document.createElement('div');
+    movieRating.classList.add('movie-card__rating');
+
+    movieHeader.appendChild(movieHeading);
+    movieHeader.appendChild(movieRating);
+
+    movieInfo.appendChild(movieHeader);
+    movieCard.appendChild(movieInfo);
+    
+    cardContainer.appendChild(movieCard);
+
+    
+}
    
